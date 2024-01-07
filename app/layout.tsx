e9a338925402c6446ac83f4034a238fbb0a264f8
George Waller <george.waller3@gmail.com>
2024-01-07 21:04:04 +0000

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         </div>
         {children}
         <ToastContainer />
+        <Analytics />
       </body>
     </html>
   );
